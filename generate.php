100163ae2f8878a80e30511d607d08137543e20d
Andrew <andy.sync@hayhillock.co.uk>
2015-01-07 22:57:48 +0000

    Updated to recurse subfolders for more files to cache

--- generate.php
+++ generate.php
@@ -8,12 +8,25 @@ echo "# Andrew Stalker 2015 \n\n";
 echo "# Explicitly cached files \n";
 
 $dir = dirname(__FILE__);
-//echo "# ".$dir. "\n";
+$dirlength = strlen($dir)+1;
+// This is the length of the site root from the manifest file.
+// This part of the path and the proceeding / (reason for the +1) must be removed to give relative link to file.
 
-$files = array_diff(scandir($dir), array('.', '..'));
-foreach ($files as $file) {
-    echo "$file\n";
+printFiles($dir,$dirlength);
+
+function printFiles($dir,$rootlength)
+{
+    $files = array_diff(scandir($dir), array('.', '..'));
+    foreach ($files as $file) {
+        (is_dir("$dir/$file")) ? printFiles("$dir/$file",$rootlength) : printFile("$dir/$file",$rootlength);
+    }
+}
+
+function printFile($file,$rootlength)
+{
+    echo substr($file,$rootlength)."\n";
     echo "#".md5_file($file)."\n";
 }
 
 ob_flush();
+
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>Test Generator page</title>
-    <link rel="stylesheet" href="style.css"
+    <link rel="stylesheet" href="style.css" />
 </head>
 <body>
 <p>This is a test page. It doesn't do anything, its just another file to cache. When using a browser debugger you
